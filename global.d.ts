7c0213f8f66e2fcc7ad4c1fdf37d1035d9c6ceba
Danny-Yellow <dzheltyxin@gmail.com>
2025-01-28 22:25:35 +0700

    init

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export = classes;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import styles from '@src/App.module.scss';
+
+export const App = () => {
+	return (
+		<>
+			<h1 className={styles.title}>Hello world!</h1>
+		</>
+	);
+};
