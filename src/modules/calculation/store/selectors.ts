1a2af3992cd996aaa54f306905b9056e163a8b02
Danny-Yellow <dzheltyxin@gmail.com>
2025-03-25 00:08:32 +0700

    fix: warning for the point selection selector

--- src/modules/calculation/store/selectors.ts
+++ src/modules/calculation/store/selectors.ts
@@ -1,13 +1,18 @@
 import type { RootState } from '@src/store';
 
+import { createSelector } from '@reduxjs/toolkit';
+
+const selectCalculationDelivery = (state: RootState) => state.calculationDelivery;
+
 export const selectPoints = (state: RootState) => state.calculationDelivery.points;
 
-export const selectSelectedPoints = (state: RootState) => {
-	return {
-		receiverPoint: state.calculationDelivery.selectedReiceiverPoint,
-		senderPoint: state.calculationDelivery.selectedSenderPoint,
-	};
-};
+export const selectSelectedPoints = createSelector(
+	[selectCalculationDelivery],
+	(calculationDelivery) => ({
+		receiverPoint: calculationDelivery.selectedReiceiverPoint,
+		senderPoint: calculationDelivery.selectedSenderPoint,
+	}),
+);
 
 export const selectPackageTypes = (state: RootState) => state.calculationDelivery.packageTypes;
 
